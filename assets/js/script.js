8dda5a93884dd072155c98245d396033cfc6781c
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-03-07 17:43:42 -0300

    correct button notes

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// let list = document.querySelectorAll('.list-item');
+// let listArray = Array.from(list);
+
+
+
+// listArray.forEach((item) =>{
+//     item.addEventListener('click', (el) =>{           
+//         el.target.classList.toggle('active');
+//     })
+// })
+
+
+// listArray.forEach((item) => {
+//     const listActive = document.querySelectorAll('active')
+//     listActive.classList.remove('active');
+// })
+
+
+
