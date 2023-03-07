78cca90e92a4cff1980cc6bd697f4911c0730364
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-03-07 13:27:28 -0300

    create mobile responsive

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const listItems = document.querySelectorAll('.list-item');
-let item = ''
-
-listItems.forEach(item => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('selected');
-  });
-});
