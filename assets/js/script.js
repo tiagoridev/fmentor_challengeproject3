41983aa9bdbef42e40e7a82aed473807c6adc5dc
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-03-08 19:42:38 -0300

    finish script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,22 @@
 const notes = document.querySelectorAll('.list-item');
-const lista =document.querySelector('#container-thanks')
 const btn = document.querySelector('#container-btn')
-const finalNote = document.querySelector('#finalNote')
-let selectedNote;
+let selectedNote = null;
 
 notes.forEach(note => {
     note.addEventListener('click', () => { 
-        console.log(`${note.textContent}`);    
+        selectedNote = note.textContent;       
 })
 })
 
-
-
-
-
-// let notes = document.querySelectorAll('.list-item');
-// const btn = document.querySelector('#container-btn');
-// const finalNote = document.querySelector('#finalNote');
-
-// let selectedNote = '';
-
-// notes.forEach((note) => {
-//   note.addEventListener('click', () => {
-//     selectedNote = note.textContent;
-//     console.log(`Botao selecionado: ${selectedNote}`);
-//   });
-// });
-
-// btn.addEventListener('click', () => {
-//   if (selectedNote === '') {
-//     alert('Por favor, selecione um número');
-//   } else {
-//     window.location.href = 'thank.html';
-//     finalNote.textContent = '5874';
-//   }
-// });
-
-
-
- // infNote.innerHTML = 'TESTE';
-
-
-// notes.forEach((note) => {
-//     note.addEventListener('click', ()=> {   
-//     // const noteA = note.innerText    
-//     console.log(`Botao selecionado: ${note.textContent}`);  
-// })  
-
-// btn.addEventListener('click', ()=> {   
-//     window.location.href = 'thank.html';    
-// })
-
-// })
-
-
-// function submit() {
-//     if (notes.length === 0){
-//         alert('Select a number')
-//     } else {
-//         const win = window.open(url)
-//         win.focus()
-//     }
-
-//     btn.addEventListener('click', submit)
-// }
-
+btn.addEventListener('click', () => {
+    if(selectedNote !== null ){    
+    document.querySelector('.container-back').classList.remove('hidden');
+    document.querySelector('.container-front').classList.add('hidden');
+    document.querySelector('#container-note').textContent = `You selected ${selectedNote} out of 5`;
+    } else{
+        alert('Please, select a note.')
+    }
+})
 
 
 
