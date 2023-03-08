6bc3744ea01b908691e43334fd4fe49a4d7e6045
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-03-08 16:00:26 -0300

    update index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,77 @@
-// let list = document.querySelectorAll('.list-item');
-// let listArray = Array.from(list);
+const notes = document.querySelectorAll('.list-item');
+const lista =document.querySelector('#container-thanks')
+const btn = document.querySelector('#container-btn')
+const finalNote = document.querySelector('#finalNote')
+let selectedNote;
+
+notes.forEach(note => {
+    note.addEventListener('click', () => { 
+        console.log(`${note.textContent}`);    
+})
+})
 
 
 
-// listArray.forEach((item) =>{
-//     item.addEventListener('click', (el) =>{           
-//         el.target.classList.toggle('active');
-//     })
+
+
+// let notes = document.querySelectorAll('.list-item');
+// const btn = document.querySelector('#container-btn');
+// const finalNote = document.querySelector('#finalNote');
+
+// let selectedNote = '';
+
+// notes.forEach((note) => {
+//   note.addEventListener('click', () => {
+//     selectedNote = note.textContent;
+//     console.log(`Botao selecionado: ${selectedNote}`);
+//   });
+// });
+
+// btn.addEventListener('click', () => {
+//   if (selectedNote === '') {
+//     alert('Por favor, selecione um número');
+//   } else {
+//     window.location.href = 'thank.html';
+//     finalNote.textContent = '5874';
+//   }
+// });
+
+
+
+ // infNote.innerHTML = 'TESTE';
+
+
+// notes.forEach((note) => {
+//     note.addEventListener('click', ()=> {   
+//     // const noteA = note.innerText    
+//     console.log(`Botao selecionado: ${note.textContent}`);  
+// })  
+
+// btn.addEventListener('click', ()=> {   
+//     window.location.href = 'thank.html';    
+// })
+
 // })
 
 
-// listArray.forEach((item) => {
-//     const listActive = document.querySelectorAll('active')
-//     listActive.classList.remove('active');
-// })
+// function submit() {
+//     if (notes.length === 0){
+//         alert('Select a number')
+//     } else {
+//         const win = window.open(url)
+//         win.focus()
+//     }
+
+//     btn.addEventListener('click', submit)
+// }
+
+
+
+
+ 
+
+
+
 
 
 
